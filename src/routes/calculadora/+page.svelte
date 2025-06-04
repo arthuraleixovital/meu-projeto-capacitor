371d419ce1c0c85c752919ea2ae8f952a524090e
arthuraleixovital <arthuraleixovital@outlook.com.br>
2025-06-04 11:04:21 -0400

    3

--- src/routes/calculadora/+page.svelte
+++ src/routes/calculadora/+page.svelte
@@ -1,20 +1,29 @@
 <script>
+    let display = $state('');
 
-    function press(){
+    function press(c){
+        display+= c;
+    }
 
+    function tirar(c){
+        display = display.slice(0, -1);
     }
 
     function calc(){
-
+        try {
+            display = eval(display.replaceAll('x', '*'));
+        } catch (error) {''
+            display = 'Error';
+        }
     }
 
     function clear(){
-        
+        display = '';
     }
 </script>
 
-<div class="text-center mt-3">
-<input type="text" class="form-control form-control-lg"  readonly>
+<div claleixolindoss="text-center mt-3">
+<input type="text" class="form-control form-control-lg"  readonly bind:value={display}>
 <table class="table table-sm table-borderless">
     <tbody>
         <tr>
@@ -27,7 +36,7 @@
             <td><button type="button" class="btn btn-primary w-100" style="background-color: #142738; border-color: #142738; height: 80px" onclick={()=>press(7)}>7</button></td>
             <td><button type="button" class="btn btn-primary w-100" style="background-color: #142738; border-color: #142738; height: 80px" onclick={()=>press(8)}>8</button></td>
             <td><button type="button" class="btn btn-primary w-100" style="background-color: #142738; border-color: #142738; height: 80px" onclick={()=>press(9)}>9</button></td>
-            <td><button type="button" class="btn btn-info w-100" style="background-color: #fcae11; border-color: #fcae11; height: 80px" onclick={()=>press('*')}>X</button></td>
+            <td><button type="button" class="btn btn-info w-100" style="background-color: #fcae11; border-color: #fcae11; height: 80px" onclick={()=>press('x')}>X</button></td>
         </tr>
         <tr>
             <td><button type="button" class="btn btn-primary w-100" style="background-color: #142738; border-color: #142738; height: 80px" onclick={()=>press(4)}>4</button></td>
@@ -40,11 +49,13 @@
             <td><button type="button" class="btn btn-primary w-100" style="background-color: #142738; border-color: #142738; height: 80px" onclick={()=>press(2)}>2</button></td>
             <td><button type="button" class="btn btn-primary w-100" style="background-color: #142738; border-color: #142738; height: 80px" onclick={()=>press(3)}>3</button></td>
             <td rowspan="2" style="height: 0px"><button type="button" class="btn btn-info w-100 h-100" style="background-color: #fcae11; border-color: #fcae11"onclick={()=>press('+')} >+</button></td>
+            <td><button type="button" class="btn btn-info w-100" style="background-color: #fcae11; border-color: #fcae11; height: 80px" onclick={()=>press('Math.sqrt()')}>√</button></td>
         </tr>
         <tr>
             <td><button type="button" class="btn btn-primary w-100" style="background-color: #142738; border-color: #142738; height: 80px"onclick={()=>press(0)} >0</button></td>
             <td><button type="button" class="btn btn-info w-100" style="background-color: #142738; border-color: #142738; height: 80px; color: white" onclick={()=>press('.')}>.</button></td>
             <td><button type="button" class="btn btn-info w-100" style="background-color: #fcae11; border-color: #fcae11; height: 80px" onclick={()=>calc()}>=</button></td>
+            <td><button type="button" class="btn btn-info w-100" style="background-color: #fcae11; border-color: #fcae11; height: 80px" onclick={()=>tirar()}>⌫</button></td>
         </tr>
     </tbody>
 </table>
