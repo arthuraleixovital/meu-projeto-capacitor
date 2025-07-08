e367b589f8dacdc226c6f59beff30692a6201d3e
arthuraleixovital <arthuraleixovital@outlook.com.br>
2025-07-08 08:26:09 -0400

    Prova corrigida.

---
+++ src/routes/prova-jogodavelha/+page.svelte
@@ -0,0 +1,131 @@
+<script>
+	let campos = $state(['', '', '', '', '', '', '', '', '']);
+	let valor = $state('X');
+	let erro = $state('');
+
+	function preencher(param) {
+		if (campos[param] == 'X') {
+			alert('Este campo já foi preenchido!');
+		}
+		if (campos[param] == 'O') {
+			alert('Este campo já foi preenchido!');
+		}
+		if (campos[param] == '') {
+			campos[param] = valor;
+			valor = 'O';
+			erro = '';
+		}
+		if (campos[param] == 'O') {
+			valor = 'X';
+			erro = '';
+		}
+	}
+
+	function reiniciar() {
+		campos = ['', '', '', '', '', '', '', '', ''];
+	}
+</script>
+
+<div class="container">
+	<h1>Jogo da Velha</h1>
+	<center>
+		<table style="row">
+			<tbody>
+				<tr>
+					<td
+						><button
+							onclick={() => preencher(0)}
+							class="btn btn-light"
+							style="height: 50px; width: 50px"><strong>{campos[0]}</strong></button
+						></td
+					>
+					<td
+						><button
+							onclick={() => preencher(1)}
+							class="btn btn-light"
+							style="height: 50px; width: 50px"><strong>{campos[1]}</strong></button
+						></td
+					>
+					<td
+						><button
+							onclick={() => preencher(2)}
+							class="btn btn-light"
+							style="height: 50px; width: 50px"><strong>{campos[2]}</strong></button
+						></td
+					>
+				</tr>
+			</tbody>
+			<tbody>
+				<tr>
+					<td
+						><button
+							onclick={() => preencher(3)}
+							class="btn btn-light"
+							style="height: 50px; width: 50px"><strong>{campos[3]}</strong></button
+						></td
+					>
+					<td
+						><button
+							onclick={() => preencher(4)}
+							class="btn btn-light"
+							style="height: 50px; width: 50px"><strong>{campos[4]}</strong></button
+						></td
+					>
+					<td
+						><button
+							onclick={() => preencher(5)}
+							class="btn btn-light"
+							style="height: 50px; width: 50px"><strong>{campos[5]}</strong></button
+						></td
+					>
+				</tr>
+			</tbody>
+			<tbody>
+				<tr>
+					<td
+						><button
+							onclick={() => preencher(6)}
+							class="btn btn-light"
+							style="height: 50px; width: 50px"><strong>{campos[6]}</strong></button
+						></td
+					>
+					<td
+						><button
+							onclick={() => preencher(7)}
+							class="btn btn-light"
+							style="height: 50px; width: 50px"><strong>{campos[7]}</strong></button
+						></td
+					>
+					<td
+						><button
+							onclick={() => preencher(8)}
+							class="btn btn-light"
+							style="height: 50px; width: 50px"><strong>{campos[8]}</strong></button
+						></td
+					>
+				</tr>
+			</tbody>
+		</table>
+	</center>
+	<br />
+	<button onclick={reiniciar} class="btn btn-danger">Reiniciar o jogo</button>
+</div>
+
+<style>
+	* {
+		font-family: sans-serif;
+	}
+	.container {
+		margin-top: 20px;
+		width: 90%;
+		max-width: 400px;
+		background: white;
+		padding: 20px;
+		border-radius: 10px;
+		box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+		text-align: center;
+	}
+	button {
+		box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+	}
+</style>
---
+++ src/routes/velhaprofessor/+page.svelte
@@ -0,0 +1,76 @@
+<script>
+	import { Button } from "bootstrap";
+
+	let tabuleiro = $state(['', '', '', '', '', '', '', '', '']);
+	let jogadaAtual = 'X';
+
+	function jogada(i) {
+		if (tabuleiro[i] != '') {
+			alert('Casa já ocupada!');
+			return;
+		}
+
+		tabuleiro[i] = jogadaAtual;
+		if (jogadaAtual == 'X') jogadaAtual = 'O';
+		else jogadaAtual = 'X';
+	}
+
+	function reiniciar() {
+		tabuleiro = ['', '', '', '', '', '', '', '', ''];
+		jogadaAtual = 'X';
+	}
+</script>
+
+<div class="mt-3">
+	<center>
+		<h1>Jogo da Velha</h1>
+		<button onclick={reiniciar} class="btn btn-primary my-3">Reiniciar</button>
+		<table>
+			<tbody>
+				<tr>
+					<td onclick={() => jogada(0)} class="borda-direita borda-inferior">{tabuleiro[0]}</td>
+					<td onclick={() => jogada(1)} class="borda-direita borda-inferior">{tabuleiro[1]}</td>
+					<td onclick={() => jogada(2)} class="borda-inferior">{tabuleiro[2]}</td>
+				</tr>
+				<tr>
+					<td onclick={() => jogada(3)} class="borda-direita borda-inferior">{tabuleiro[3]}</td>
+					<td onclick={() => jogada(4)} class="borda-direita borda-inferior">{tabuleiro[4]}</td>
+					<td onclick={() => jogada(5)} class="borda-inferior">{tabuleiro[5]}</td>
+				</tr>
+				<tr>
+					<td onclick={() => jogada(6)} class="borda-direita">{tabuleiro[6]}</td>
+					<td onclick={() => jogada(7)} class="borda-direita">{tabuleiro[7]}</td>
+					<td onclick={() => jogada(8)}>{tabuleiro[8]}</td>
+				</tr>
+			</tbody>
+		</table>
+	</center>
+</div>
+
+<style>
+	h1 {
+		font-size: 10dvw;
+	}
+	button {
+		width: 20vw;
+		height: 8dvw;
+		font-size: 3dvw;
+		text-align: center;
+	}
+	table {
+		width: 60dvw;
+		height: 60dvw;
+		text-align: center;
+		font-size: 7dvw;
+	}
+	td {
+		width: 33%;
+		height: 33%;
+	}
+	.borda-direita {
+		border-right: 1dvw solid black;
+	}
+	.borda-inferior {
+		border-bottom: 1dvw solid black;
+	}
+</style>
